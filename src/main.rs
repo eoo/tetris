c0a96b7dae0f8416f7fcff99e6bd8fdfff37b88a
tarun singh <tarungarhwal@gmail.com>
2025-10-07 02:02:51 +0530

    iterator, save high score

--- src/main.rs
+++ src/main.rs
@@ -54,6 +54,16 @@ fn read_from_file(file_name: &str) -> io::Result<String> {
     Ok(content)
 }
 
+fn slice_to_string(slice: &[u32]) -> String {
+    slice.iter().map(|highscore| highscore.to_string()).collect::<Vec<_>>().join(" ")
+}
+
+fn save_highscore_and_lines(highscores: &[u32], number_of_lines: &[u32]) -> bool {
+    let s_highscores = slice_to_string(highscores);
+    let s_number_of_lines = slice_to_string(number_of_lines);
+
+    write_into_file(&format!("{}\n{}\n", s_highscores, s_number_of_lines), "highscores.txt").is_ok()
+}
 
 pub fn main() {
     let sdl_context = sdl3::init().expect("SDL initialization failed");
