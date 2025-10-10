6b634835b66aae91e3cc3c8fde9bec1fa430f5de
tarun singh <tarungarhwal@gmail.com>
2025-10-11 02:40:32 +0530

    tetriminos

--- src/main.rs
+++ src/main.rs
@@ -90,6 +90,226 @@ fn load_highscores_and_lines() -> Option<(Vec<u32>, Vec<u32>)> {
 }
 
 
+type Piece = Vec<Vec<u8>>;
+type States = Vec<Piece>;
+
+struct Tetrimino {
+    states: States,
+    x: isize,
+    y: usize,
+    current_state: u8,
+}
+
+trait TetriminoGenerator {
+    fn new() -> Tetrimino;
+}
+
+struct TetriminoI;
+impl TetriminoGenerator for TetriminoI {
+    fn new() -> Tetrimino {
+        Tetrimino {
+            states: vec![
+                        vec![
+                            vec![1, 1, 1, 1],
+                            vec![0, 0, 0, 0],
+                            vec![0, 0, 0, 0],
+                            vec![0, 0, 0, 0],
+                        ],
+                        vec![
+                            vec![0, 1, 0, 0],
+                            vec![0, 1, 0, 0],
+                            vec![0, 1, 0, 0],
+                            vec![0, 1, 0, 0],
+                        ]
+                    ],
+            x: 4,
+            y: 0,
+            current_state: 0,
+        }
+    }
+}
+
+struct TetriminoL;
+impl TetriminoGenerator for TetriminoL {
+    fn new() -> Tetrimino {
+        Tetrimino {
+            states: vec![
+                        vec![
+                            vec![2, 2, 2, 0],
+                            vec![2, 0, 0, 0],
+                            vec![0, 0, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                        vec![
+                            vec![2, 2, 0, 0],
+                            vec![0, 2, 0, 0],
+                            vec![0, 2, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                        vec![
+                            vec![0, 0, 2, 0],
+                            vec![2, 2, 2, 0],
+                            vec![0, 0, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                        vec![
+                            vec![2, 0, 0, 0],
+                            vec![2, 0, 0, 0],
+                            vec![2, 2, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ]
+                    ],
+                x: 4,
+                y: 0,
+                current_state: 0,
+        }
+    }
+}
+
+struct TetriminoJ;
+impl TetriminoGenerator for TetriminoJ {
+    fn new() -> Tetrimino {
+        Tetrimino {
+            states: vec![
+                        vec![
+                            vec![3, 3, 3, 0],
+                            vec![0, 0, 3, 0],
+                            vec![0, 0, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                        vec![
+                            vec![0, 3, 0, 0],
+                            vec![0, 3, 0, 0],
+                            vec![3, 3, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                        vec![
+                            vec![3, 0, 0, 0],
+                            vec![3, 3, 3, 0],
+                            vec![0, 0, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                        vec![
+                            vec![3, 3, 0, 0],
+                            vec![3, 0, 0, 0],
+                            vec![3, 0, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ]
+                    ],
+                x: 4,
+                y: 0,
+                current_state: 0,
+        }
+    }
+}
+
+struct TetriminoO;
+impl TetriminoGenerator for TetriminoO {
+    fn new() -> Tetrimino {
+        Tetrimino {
+            states: vec![
+                        vec![
+                            vec![4, 4, 0, 0],
+                            vec![4, 4, 0, 0],
+                            vec![0, 0, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                    ],
+                x: 4,
+                y: 0,
+                current_state: 0,
+        }
+    }
+}
+
+struct TetriminoS;
+impl TetriminoGenerator for TetriminoS {
+    fn new() -> Tetrimino {
+        Tetrimino {
+            states: vec![
+                        vec![
+                            vec![0, 5, 5, 0],
+                            vec![5, 5, 0, 0],
+                            vec![0, 0, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                        vec![
+                            vec![0, 5, 0, 0],
+                            vec![0, 5, 5, 0],
+                            vec![0, 0, 5, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                    ],
+                x: 4,
+                y: 0,
+                current_state: 0,
+        }
+    }
+}
+
+struct TetriminoZ;
+impl TetriminoGenerator for TetriminoZ {
+    fn new() -> Tetrimino {
+        Tetrimino {
+            states: vec![
+                        vec![
+                            vec![6, 6, 0, 0],
+                            vec![0, 6, 6, 0],
+                            vec![0, 0, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                        vec![
+                            vec![0, 0, 6, 0],
+                            vec![0, 6, 6, 0],
+                            vec![0, 6, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                    ],
+                x: 4,
+                y: 0,
+                current_state: 0,
+        }
+    }
+}
+
+struct TetriminoT;
+impl TetriminoGenerator for TetriminoT {
+    fn new() -> Tetrimino {
+        Tetrimino {
+            states: vec![
+                        vec![
+                            vec![7, 7, 7, 0],
+                            vec![0, 7, 0, 0],
+                            vec![0, 0, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                        vec![
+                            vec![0, 7, 0, 0],
+                            vec![7, 7, 0, 0],
+                            vec![0, 7, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                        vec![
+                            vec![0, 7, 0, 0],
+                            vec![7, 7, 7, 0],
+                            vec![0, 0, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ],
+                        vec![
+                            vec![0, 7, 0, 0],
+                            vec![0, 7, 7, 0],
+                            vec![0, 7, 0, 0],
+                            vec![0, 0, 0, 0]
+                        ]
+                    ],
+                x: 4,
+                y: 0,
+                current_state: 0,
+        }
+    }
+}
+
+
 
 
 pub fn main() {
